5723cbc3916d3672276394f32ca8e99efeec0db9
lokkazzy <jiongsoul@gmail.com>
2023-05-29 20:33:40 +0200

    Get movie info

---
+++ js/MovieInfo.js
@@ -0,0 +1,131 @@
+﻿const infoUri = 'http://sep-db-386814.ew.r.appspot.com/movie/getOne?id=';
+var detailedData = '';
+const actorsUri = 'http://sep-db-386814.ew.r.appspot.com/people/getActors?movieTitle=';
+var actorsJson = '';
+var movieId = 0;
+const imageUri = 'http://sep-db-386814.ew.r.appspot.com/movie/getImage?id=00';
+var imageJson = '';
+const ratingUri = 'http://sep-db-386814.ew.r.appspot.com/ratings/getRating?movieId=';
+var ratingJson = '';
+const directorUri = 'http://sep-db-386814.ew.r.appspot.com/people/getDirectorById?movieId=';
+var directorJson = '';
+const commentUri = 'http://sep-db-386814.ew.r.appspot.com/comment/get/movie?mId=';
+var commentJson = '';
+
+function GetMovieInfo(){
+  GetId();
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${infoUri}${movieId}`, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+      console.log(xhr.responseText)
+      var data = xhr.responseText;
+      detailedData = JSON.parse(data); //id,title,year
+      GetActors();
+      GetImage();
+      GetRating();
+      GetDirector();
+      GetComment();
+    }
+  }
+}
+
+function GetId()
+{
+  var map = new Map();
+  var receiveData = decodeURI(window.location.search);
+  var key=receiveData.substring(receiveData.indexOf("?")+1,receiveData.indexOf("="));
+  var value=receiveData.substring(receiveData.indexOf("=")+1);
+  map.set(key,value);
+  movieId = value;
+console.log(movieId);
+}
+
+function GetActors()
+{
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${actorsUri}${detailedData.title}&pageNum=1&pageSize=100`, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+      console.log(xhr.responseText)
+      var data = xhr.responseText;
+      actorsJson = JSON.parse(data);
+    }
+  }
+}
+
+function GetImage()
+{
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${imageUri}${movieId}`, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+      console.log(xhr.responseText)
+      var  data = xhr.responseText;
+      imageJson = JSON.parse(data);
+    }
+  }
+}
+
+function GetRating()
+{
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${ratingUri}${movieId}`, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+      console.log(xhr.responseText)
+      var  data = xhr.responseText;
+      ratingJson = JSON.parse(data);
+    }
+  }
+}
+
+function GetDirector()
+{
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${directorUri}${movieId}`, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+      console.log(xhr.responseText)
+      var  data = xhr.responseText;
+      directorJson = JSON.parse(data);
+    }
+  }
+}
+
+function GetComment()
+{
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${commentUri}${movieId}`, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+      console.log(xhr.responseText)
+      var  data = xhr.responseText;
+      commentJson = JSON.parse(data);
+    }
+  }
+}
--- js/Movies.js
+++ js/Movies.js
@@ -1,13 +1,14 @@
 ﻿var pageNum = 1; //页数
 var pageSize = 12; //每页电影数
 const uri = 'http://sep-db-386814.ew.r.appspot.com/movie/getAll?pageNum=';
-const infoUri = 'http://sep-db-386814.ew.r.appspot.com/movie/getOne?id=';
+var clickedId = 0;
 var movieId = 0;
 const ratingUri = 'http://sep-db-386814.ew.r.appspot.com/ratings/getRating?movieId=';
-var ratingJson;
-const actorsUri = 'http://sep-db-386814.ew.r.appspot.com/people/getActors?movieTitle=';
+var ratingJson = '';
 const searchUri = 'http://sep-db-386814.ew.r.appspot.com/movie/getByIdTitle?pageNum=1&pageSize=12&id=0&title=';
 var searchTitle = '';
+const imageUri = 'http://sep-db-386814.ew.r.appspot.com/movie/getImage?id=00';
+var imageJson = '';
 
 
 
@@ -58,33 +59,6 @@ for (let i = 1; i <= 12; i++) {
   ratings.push(rating);
 }
 
-
-
-function GetMoviesForIndex(){
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', `${uri}${pageNum}&pageSize=${pageSize}`, true)
-  xhr.setRequestHeader('Content-Type', 'application/json')
-  xhr.setRequestHeader('Accept', 'application/json')
-
-  xhr.send()
-  xhr.onreadystatechange = () => {
-    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
-      console.log(xhr.responseText)
-      var  data = xhr.responseText;
-      var json=JSON.parse(data); //json格式电影信息
-
-      for (let i = 0; i < 12; i++) {
-        names[i].innerHTML=json.list[i].title;
-        times[i].innerHTML="Opened "+json.list[i].year;
-
-        //rating part
-        movieId = json.list[i].id;
-        GetRatings(); //ratingJson包含评分信息
-      }
-    }
-  }
-}
-
 function GetMovies(){
   const xhr = new XMLHttpRequest();
   xhr.open('GET', `${uri}${pageNum}&pageSize=${pageSize}`, true)
@@ -104,9 +78,9 @@ function GetMovies(){
     var element = document.elementFromPoint(e.clientX,e.clientY);
     if (element.classList.contains("movie"))
     {
-      movieId = element.id;
+      clickedId = element.id;
       GetMovieInfo();
-      window.location.href = "../movieInfo.html";
+      window.location.href = "../movieInfo.html?id=" + clickedId;
     }
   }
 
@@ -142,7 +116,7 @@ function GetMovies(){
   }
 }
 
-function GetRatings()
+function GetRating()
 {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', `${ratingUri}${movieId}`, true)
@@ -154,13 +128,29 @@ function GetRatings()
     if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
       console.log(xhr.responseText)
       var  data = xhr.responseText;
-      var ratingData = JSON.parse(data);
-      ratingJson = ratingData;
+      ratingJson = JSON.parse(data);
     }
   }
 }
 
-function Search() //使用时更改searchTitle参数
+function GetImage()
+{
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${imageUri}${movieId}`, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+      console.log(xhr.responseText)
+      var  data = xhr.responseText;
+      imageJson = JSON.parse(data);
+    }
+  }
+}
+
+function Search() //调用前更改searchTitle参数
 {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', `${searchUri}${searchTitle}`, true)
