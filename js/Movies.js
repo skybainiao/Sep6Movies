dc4dad6460aa2f02a368ca977965159a40ff9219
lokkazzy <jiongsoul@gmail.com>
2023-05-29 13:11:09 +0200

    Rating

--- js/Movies.js
+++ js/Movies.js
@@ -1,6 +1,10 @@
 ﻿var pageNum = 1; //页数
 var pageSize = 12; //每页电影数
 const uri = 'http://sep-db-386814.ew.r.appspot.com/movie/getAll?pageNum=';
+const infoUri = 'http://sep-db-386814.ew.r.appspot.com/movie/getOne?id=';
+var movieId = 0;
+const ratingUri = 'sep-db-386814.ew.r.appspot.com/ratings/getRating?movieId=';
+var ratingJson;
 
 //给index用的
 let imgs = [];
@@ -42,28 +46,13 @@ function GetMoviesForIndex(){
         names[i].innerHTML=json.list[i].title;
         times[i].innerHTML="Opened "+json.list[i].year;
 
+        //rating part
+        movieId = json.list[i].id;
+        GetRatings(); //ratingJson包含评分信息
       }
-
-
-
-
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function GetMovies(){
   const xhr = new XMLHttpRequest();
@@ -77,10 +66,50 @@ function GetMovies(){
       console.log(xhr.responseText)
       var  data = xhr.responseText;
       var json=JSON.parse(data); //json格式电影信息
+    }
+  }
 
+  window.onclick = function (e) {
+    var element = document.elementFromPoint(e.clientX,e.clientY);
+    if (element.classList.contains("movie"))
+    {
+      movieId = element.id;
+      GetMovieInfo();
+      window.location.href = "../movieInfo.html";
+    }
+  }
 
+  function GetMovieInfo(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `${infoUri}${movieId}`, true)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.setRequestHeader('Accept', 'application/json')
 
+    xhr.send()
+    xhr.onreadystatechange = () => {
+      if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+        console.log(xhr.responseText)
+        var data = xhr.responseText;
+        var detailedData = JSON.parse(data);
+      }
+    }
+  }
+}
 
+function GetRatings()
+{
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${ratingUri}${movieId}`, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+      console.log(xhr.responseText)
+      var  data = xhr.responseText;
+      var ratingData = JSON.parse(data);
+      ratingJson = ratingData;
     }
   }
 }
