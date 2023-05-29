2e3ff744ac61a933399917566f27c9df9a110c2d
lokkazzy <jiongsoul@gmail.com>
2023-05-29 13:26:05 +0200

    Search function

--- js/Movies.js
+++ js/Movies.js
@@ -3,8 +3,11 @@ var pageSize = 12; //每页电影数
 const uri = 'http://sep-db-386814.ew.r.appspot.com/movie/getAll?pageNum=';
 const infoUri = 'http://sep-db-386814.ew.r.appspot.com/movie/getOne?id=';
 var movieId = 0;
-const ratingUri = 'sep-db-386814.ew.r.appspot.com/ratings/getRating?movieId=';
+const ratingUri = 'http://sep-db-386814.ew.r.appspot.com/ratings/getRating?movieId=';
 var ratingJson;
+const actorsUri = 'http://sep-db-386814.ew.r.appspot.com/people/getActors?movieTitle=';
+const searchUri = 'http://sep-db-386814.ew.r.appspot.com/movie/getByIdTitle?pageNum=1&pageSize=12&id=0&title=';
+var searchTitle = '';
 
 //给index用的
 let imgs = [];
@@ -90,7 +93,22 @@ function GetMovies(){
       if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
         console.log(xhr.responseText)
         var data = xhr.responseText;
-        var detailedData = JSON.parse(data);
+        var detailedData = JSON.parse(data); //id,title,year
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `${actorsUri}${detailedData.title}&pageNum=1&pageSize=100`, true)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.setRequestHeader('Accept', 'application/json')
+
+        xhr.send()
+        xhr.onreadystatechange = () => {
+          if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
+            console.log(xhr.responseText)
+            var data = xhr.responseText;
+            var actorsData = JSON.parse(data); //actors info
+
+          }
+        }
       }
     }
   }
@@ -110,6 +128,23 @@ function GetRatings()
       var  data = xhr.responseText;
       var ratingData = JSON.parse(data);
       ratingJson = ratingData;
+    }
+  }
+}
+
+function Search() //使用时更改searchTitle参数
+{
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${searchUri}${searchTitle}`, true)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.setRequestHeader('Accept', 'application/json')
+
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && /^20\d$/.test(xhr.status)) {
+      console.log(xhr.responseText)
+      var data = xhr.responseText;
+      var json = JSON.parse(data); //输出
     }
   }
 }
