199343a29e9dc822d7f3ae1c4a0216f7282534a4
lokkazzy <jiongsoul@gmail.com>
2023-05-30 14:58:32 +0200

    login

--- js/Login.js
+++ js/Login.js
@@ -1,32 +1,27 @@
 ﻿var username = "";
 var password = "";
-const uri = 'http://sep-db-386814.ew.r.appspot.com/user/getUser?id=0&username=';
+const uri = 'http://sep-db-386814.ew.r.appspot.com/user/login?username=';
 
 function Login(){
   username = document.getElementById("username").value;
   password = document.getElementById("password").value;
 
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', `${uri}${username}`, true)
+  xhr.open('GET', `${uri}${username}&password=${password}`, true)
   xhr.setRequestHeader('Content-Type', 'application/json')
   xhr.setRequestHeader('Accept', 'application/json')
   xhr.send()
   xhr.onreadystatechange = () => {
     //获取响应内容
-    if(xhr.readyState === 4 && /^20\d$/.test(xhr.status)){
+    if(xhr.readyState == 4 && /^20\d$/.test(xhr.status)){
       console.log(xhr.responseText)
       var  data = xhr.responseText;
       var json=JSON.parse(data);
 
-      if (json.password.toString() !== password)
+      alert(json);
+      if (json.toString() == 'Login success')
       {
-        alert("invalid account")
-      }
-      else
-      {
-        alert("login successful")
-        localStorage.setItem('username', username);
-        window.location.href = "../index.html";
+        window.location.href = "../index.html?username=" + username;
       }
     }
   }
